c97e1cf04e54c837530a9da7a4e6ce3de7ff1678
l241025097 <luo241025097@sina.com>
2017-10-03 07:27:32 +0800

    test link mongodb

--- test_mongo.py
+++ test_mongo.py
@@ -1,3 +1,9 @@
 import pymongo
+import urllib
 
-pymongo.MongoClient('mongodb://%s:%s@127.0.0.1:61111' % ('luoyl25', 'S198641cn')).get_database('oss')
+username = urllib.parse.quote_plus('admin')
+password = urllib.parse.quote_plus('Weixin@1026')
+host = '127.0.0.1:61111'
+
+dbh = pymongo.MongoClient('mongodb://%s:%s@%s' % (username, password, host)).get_database('admin')
+print(dbh.name)
